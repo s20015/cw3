1ab75320463506cd4423fe021add667e190f394a
Artur Mruk <s20015@pjwstk.edu.pl>
2020-04-05 21:31:57 +0200

    Add incomplete project

---
+++ Wyklad3/Controllers/StudentsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Wyklad3.Models;
+using Wyklad3.Services;
+
+namespace Wyklad3.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private IDbService _dbService;
+
+        public StudentsController(IDbService service)
+        {
+            _dbService = service;
+        }
+
+        //2. QueryString
+        [HttpGet]
+        public IActionResult GetStudents([FromServices]IDbService service, [FromQuery]string orderBy)
+        {
+            if (orderBy == "lastname")
+            {
+                return Ok(_dbService.GetStudents().OrderBy(s => s.LastName));
+            }
+
+            return Ok(_dbService.GetStudents());
+        }
+
+        //[FromRoute], [FromBody], [FromQuery]
+        //1. URL segment
+        [HttpGet("{id}")]
+        public IActionResult GetStudent([FromRoute]int id) //action method
+        {
+            if (id == 1)
+            {
+                return Ok("Jan");
+            }
+
+            return NotFound("Student was not found");
+        }
+
+        //3. Body - cialo zadan
+        [HttpPost]
+        public IActionResult CreateStudent([FromBody]Student student)
+        {
+            student.IndexNumber=$"s{new Random().Next(1, 20000)}";
+            //...
+            return Ok(student); //JSON
+        }
+
+
+    }
+}
